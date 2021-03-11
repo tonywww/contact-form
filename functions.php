0661cd111baaaee60c82d91252b467e946d0ee47
Tony <root@pc>
2021-03-11 13:28:40 -0500

    add #contactform section

--- functions.php
+++ functions.php
@@ -4,7 +4,7 @@ function redirectWithError($error)
 {
     $_SESSION['_contact_form_error'] = $error;
 
-    header('Location: '.$_SERVER['HTTP_REFERER']);
+    header('Location: '.$_SERVER['HTTP_REFERER'].'#contactform');
     echo "Error: ".$error;
     die();
 }
@@ -13,7 +13,7 @@ function redirectSuccess()
 {
     $_SESSION['_contact_form_success'] = true;
 
-    header('Location: '.$_SERVER['HTTP_REFERER']);
+    header('Location: '.$_SERVER['HTTP_REFERER'].'#contactform');
     echo "Your message was sent successfully!";
     die();
 }
